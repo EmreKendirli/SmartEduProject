8c71a0f336c7c849cdad722b9f041d0ff9ca1c32
EmreKendirli <emrekenderli@gmail.com>
2023-03-25 03:09:13 +0300

    ejs eklendi

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express =require("express");
  const app=express();
 
+ //Template Engine
+ app.set("view engine","ejs");
+
+//Middlewares
+app.use(express.static("public"));
+//routers
+app.get("/",(req,res)=>{
+   res.status(200).render("index",{
+      page_Name:"index"
+   });
+});
+
+app.get("/about",(req,res)=>{
+   res.status(200).render("about",{
+      page_Name:"about"
+
+   });
+})
+
  const port =3000;
  app.listen(port,()=>{
     console.log(`App started on port: ${port}`)
